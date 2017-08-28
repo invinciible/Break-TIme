e9221f7681a52bb5bb3d8e958a2fbe1fdf081783
invinciible <tktusharkatyal11@gmail.com>
2017-08-29 00:17:10 +0530

    installed firebase pod

---
+++ Break-TIme/Services/DataService.swift
@@ -0,0 +1,21 @@
+//
+//  DataService.swift
+//  Break-TIme
+//
+//  Created by Tushar Katyal on 29/08/17.
+//  Copyright © 2017 Tushar Katyal. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class DataService {
+    
+    static let instance = DataService()
+    
+    
+    
+    
+    
+    
+}

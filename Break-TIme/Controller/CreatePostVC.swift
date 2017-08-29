f3c32a43b45719b0bf2bd2f6fed37bbf9aa57127
invinciible <tktusharkatyal11@gmail.com>
2017-08-29 18:53:29 +0530

    posting to firebase complete

---
+++ Break-TIme/Controller/CreatePostVC.swift
@@ -0,0 +1,46 @@
+//
+//  CreatePostVC.swift
+//  Break-TIme
+//
+//  Created by Tushar Katyal on 29/08/17.
+//  Copyright © 2017 Tushar Katyal. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class CreatePostVC: UIViewController {
+
+    // outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func sendBtnPressed(_ sender: Any) {
+        
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUid: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isSuccess) in
+                if isSuccess {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("tush : Error posting")
+                   
+                }
+            })
+        }
+    }
+    
+}
--- Break-TIme/Services/DataService.swift
+++ Break-TIme/Services/DataService.swift
@@ -34,4 +34,26 @@ class DataService {
     func createDbUser(uid : String,userData : Dictionary<String,Any>) {
         REF_USERS.child(uid).updateChildValues(userData)
     }
+    
+    func uploadPost(withMessage message : String , forUid uid : String,withGroupKey groupKey : String?, sendComplete : @escaping (_ status :Bool)->()){
+        
+        if groupKey != nil {
+            // send this to group ref
+        } else {
+            REF_FEED.childByAutoId().updateChildValues(["senderId" : uid,"contents" : message])
+            sendComplete(true)
+        }
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+

8b17711eb15d0ea479fa0895a7b0fc9918c25ff3
invinciible <tktusharkatyal11@gmail.com>
2017-08-29 18:00:07 +0530

    profile ui completed

---
+++ Break-TIme/Controller/MeVC.swift
@@ -0,0 +1,26 @@
+//
+//  MeVC.swift
+//  Break-TIme
+//
+//  Created by Tushar Katyal on 29/08/17.
+//  Copyright © 2017 Tushar Katyal. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var profileImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+    }
+    
+}

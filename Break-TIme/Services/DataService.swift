73966e87f61c8e508ff370619d3aea4ac4d6a91a
invinciible <tktusharkatyal11@gmail.com>
2017-08-29 12:51:05 +0530

    ui complete for Auth

--- Break-TIme/Services/DataService.swift
+++ Break-TIme/Services/DataService.swift
@@ -13,9 +13,25 @@ class DataService {
     
     static let instance = DataService()
     
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
     
+    var REF_BASE : DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS : DatabaseReference {
+        return _REF_USERS
+    }
+    var REF_GROUPS : DatabaseReference {
+        return _REF_GROUPS
+    }
+    var REF_FEED : DatabaseReference{
+        return _REF_FEED
+    }
     
-    
-    
-    
+    func createDbUser(uid : String,userData : Dictionary<String,Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
 }
---
+++ Break-TIme/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Break-TIme
+//
+//  Created by Tushar Katyal on 29/08/17.
+//  Copyright © 2017 Tushar Katyal. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+// References
+let DB_BASE = Database.database().reference()
